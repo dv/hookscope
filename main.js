383d8340a4f5c128d80592b40563949cbf887f1c
David Verhasselt <david@crowdway.com>
2011-12-27 01:50:11 +0100

    Added server-side request parsing and forwarding to client.

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ io.sockets.on('connection', function(socket) {
 	socket.vars = {};
 	socket.vars["chnl"] = "MIIIAAAWWOWOO"
 	socket.on('set channel', function(channel) {
-		socket.emit("request", { RequestType: "GET", Path: "home and away."})
-		socket.emit("request", { RequestType: "POST", Path: "WHATEVER."})
 		socket.set('channel', channel, function () {
 			console.log("Creating channel " + channel);
 			cons["channel" + channel] = this;	
@@ -21,3 +19,19 @@ io.sockets.on('connection', function(socket) {
 		});
 	});
 });
+
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+
+  dataObject = {};
+  dataObject.url = url.parse(req.url, true);
+  dataObject.headers = req.headers;
+  dataObject.httpVersion = req.httpVersion;
+  dataObject.method = req.method;
+
+  io.sockets.emit("request", dataObject);
+}).listen(8001, "127.0.0.1");
